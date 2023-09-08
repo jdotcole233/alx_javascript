9786ef6b4614c5f5799e489eddd6dc538f9a0172
Jay Cole <jdotcole233@gmail.com>
2023-09-08 00:36:32 +0000

    docs

---
+++ javascript-web_scraping/2-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const characterID = 18;
+
+const chunks = [];
+
+request.get(url, { encoding: 'utf-8' })
+  .on('data', function (data) {
+    chunks.push(data);
+  })
+  .on('end', function () {
+    const data = JSON.parse(chunks);
+    const resp = data.results.map(film => film.characters).flat().filter(char => char.includes(characterID));
+    console.log(resp.length);
+  });
