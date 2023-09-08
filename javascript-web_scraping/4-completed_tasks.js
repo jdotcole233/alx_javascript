21039dc7b4a61971a74cc66be2a8eebedc75afc2
Jay Cole <jdotcole233@gmail.com>
2023-09-08 01:48:00 +0000

    docs

---
+++ javascript-web_scraping/4-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+const chunks = [];
+
+request.get(url)
+  .on('data', data => {
+    chunks.push(data);
+  })
+  .on('end', () => {
+    const bufferData = Buffer.concat(chunks);
+    const response = {};
+    JSON.parse(bufferData)
+      .forEach(todo => {
+        if (!response.prototype.hasOwnProperty.call(todo.userId)) {
+          response[todo.userId] = 0;
+        }
+
+        if (todo.completed) {
+          response[todo.userId]++;
+        }
+      });
+    console.log(response);
+  });
